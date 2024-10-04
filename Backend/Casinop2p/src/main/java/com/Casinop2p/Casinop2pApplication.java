801b584e9740bccd1141d8b02863372c1471c617
Dota43ver <facundoagueroc@gmail.com>
2024-10-04 15:40:43 -0300

    first commit

---
+++ Backend/Casinop2p/src/main/java/com/Casinop2p/Casinop2pApplication.java
@@ -0,0 +1,13 @@
+package com.Casinop2p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Casinop2pApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Casinop2pApplication.class, args);
+	}
+
+}
---
+++ Backend/Casinop2p/src/test/java/com/Casinop2p/Casinop2pApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Casinop2p;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Casinop2pApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

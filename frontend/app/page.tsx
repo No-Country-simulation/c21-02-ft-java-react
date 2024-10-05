311e4c670b569bdac5f6897d3f03a7cbfbd57a85
Bryan Lundberg <bryanlundberg@outlook.com>
2024-10-05 06:08:50 -0600

    feat: add some traditional components

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,15 @@
+import Footer from "@/components/footer/footer";
+import NavbarNotAuthenticated from "@/components/navbars/not-authenticated/navbar-not-authenticated";
+
 export default function Home() {
   return (
     <>
-      <div></div>
+      <div className="min-h-dvh overflow-hidden flex flex-col">
+        <NavbarNotAuthenticated />
+        {/* last bets */}
+        <div className="grow"></div>
+        <Footer />
+      </div>
     </>
   );
 }

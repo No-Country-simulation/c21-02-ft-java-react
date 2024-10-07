84cfffe93ff84d6309dff512baf533febd333983
Bryan Lundberg <bryanlundberg@outlook.com>
2024-10-07 13:53:31 -0600

    style: add layout for login form (#19)

---
+++ frontend/components/login-sheet-content/login-sheet-content.tsx
@@ -0,0 +1,94 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import Image from "next/image";
+import {
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "../ui/sheet";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  email: z.string(),
+  password: z.string(),
+});
+
+export default function LoginSheetContent() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <>
+      <SheetContent>
+        <SheetHeader>
+          <Image
+            src={"/logo.png"}
+            alt=""
+            width={40}
+            height={40}
+            className="mx-auto"
+          />
+          <SheetTitle className="text-center">Super Apuestas</SheetTitle>
+          <SheetDescription>
+            Accede a la plataforma con tu usuario y contraseña.
+          </SheetDescription>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Tu-correo@email.com" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Contraseña</FormLabel>
+                    <FormControl>
+                      <Input type="password" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className="w-full">
+                Submit
+              </Button>
+            </form>
+          </Form>
+        </SheetHeader>
+      </SheetContent>
+    </>
+  );
+}

b1051bc18fdc982bf7a49fe48903d7af28dcea49
Bryan Lundberg <bryanlundberg@outlook.com>
2024-10-08 05:26:29 -0600

    feat: add secondary navbar (#23)

--- frontend/app/(iniciado-sesion)/apuestas/page.tsx
+++ frontend/app/(iniciado-sesion)/apuestas/page.tsx
@@ -1,7 +1,17 @@
+import { TableBets } from "@/components/table-bets/table-bets";
+import { Input } from "@/components/ui/input";
+
 export default function Page() {
   return (
     <>
-      <div>Contenido</div>
+      <div className="max-h-screen min-h-screen overflow-auto pt-5">
+        <div className="flex justify-between max-w-5xl mx-auto">
+          <h2 className="text-4xl font-medium ms-2">Apuestas en curso</h2>
+          <Input placeholder="🔎 Buscar por nombre" className="max-w-60 me-5" />
+        </div>
+
+        <TableBets />
+      </div>
     </>
   );
 }
--- frontend/app/(iniciado-sesion)/mi-cuenta/page.tsx
+++ frontend/app/(iniciado-sesion)/mi-cuenta/page.tsx
@@ -1,7 +1,10 @@
 export default function Page() {
   return (
     <>
-      <div>Contenido</div>
+      <div className="max-h-screen min-h-screen overflow-auto pt-5">
+        <div className="mx-auto max-w-5xl ">Contenido</div>
+        Personalizar, foto de perfil, nombre usuario, etc
+      </div>
     </>
   );
 }

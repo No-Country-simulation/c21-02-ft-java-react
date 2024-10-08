29c31eb6a66ff8926df90dde0b2eee58d397430b
Bryan Lundberg <bryanlundberg@outlook.com>
2024-10-07 19:42:31 -0600

    basic routing logged in user

--- frontend/app/(iniciado-sesion)/layout.tsx
+++ frontend/app/(iniciado-sesion)/layout.tsx
@@ -1,11 +1,15 @@
 import NavbarAuthenticated from "@/components/navbars/authenticated/navbar-authenticated";
 
-export default function Page() {
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <>
       <div className="flex min-h-screen">
         <NavbarAuthenticated />
-        <div className="grow"></div>
+        <div className="grow">{children}</div>
       </div>
     </>
   );
